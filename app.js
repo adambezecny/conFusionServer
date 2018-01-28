3cfe0cebfacbbfc59d3693456031543f17b92224
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2018-01-28 19:11:47 +0100

    Express REST API with Mongoose - Part 1

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promotionRouter = require('./routes/promotionRouter');
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+// Connection URL
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, { });
+
+connect.then(
+  ()    => {console.log("Connected correctly to mongo server");}, 
+  (err) => { console.log(err); }
+);
+
 var app = express();
 
 // view engine setup
